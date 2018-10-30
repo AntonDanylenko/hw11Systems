dd0c4ed296bf11bf18614450a498d49303ad0e0c
Kerwin Chen <kchen37@stuy.edu>
2018-10-30 11:01:47 -0400

    added

--- statTest.c
+++ statTest.c
@@ -1,9 +1,12 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 int main(){
   struct stat buffer;
   stat("README.md", &buffer);
   printf("File size: %ld\n", buffer.st_size);
+  printf("mode: %d\n", buffer.st_mode);
+  printf("Tiem of Last access: %s\n", ctime(buffer.st_atime));
 }
