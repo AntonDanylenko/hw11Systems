6c0555ef9df7fed1192bbfbfa8ae173f3992ccc5
kerwinchen31 <kchen37@stuy.edu>
2018-10-30 21:17:22 -0400

    added test

--- statTest.c
+++ statTest.c
@@ -2,11 +2,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 int main(){
   struct stat buffer;
-  stat("README.md", &buffer);
-  printf("File size: %ld\n", buffer.st_size);
+  stat("test.file", &buffer);
+  int size = buffer.st_size;
+  printf("File size: %d\n", size);
   printf("mode: %d\n", buffer.st_mode);
   printf("Time of Last access: %s\n", ctime(&buffer.st_atime));
+  
+  char b[100];
+  char *s = "%ld B";
+  if(size/1000 > 0){
+    size /= 1000;
+    s = "%ld KB";
+  }
+  if(size/1000 > 0){
+    size /= 1000;
+    s = "%ld MB";
+  }
+  if(size/1000 > 0){
+    size /= 1000;
+    s = "%ld GB";
+    }
+  
+  sprintf(b, s, size);
+  printf("File size (readable): %s\n", b);
 }
