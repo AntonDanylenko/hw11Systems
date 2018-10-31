0ee381d4d46d4b8690c57e1b60aa0ca6caf12026
Anton Danylenko <adanylenko@stuy.edu>
2018-10-30 22:10:56 -0400

    s

--- statTest.c
+++ statTest.c
@@ -30,6 +30,6 @@ int main(){
   sprintf(b, s, size);
   printf("File size (readable): %s\n", b);
 
-  int permissions = (int)&buffer.st_mode;
+  int permissions = buffer.st_mode;
   printf("Permissions: %d\n", permissions);
 }
